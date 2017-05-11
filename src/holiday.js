62c732d3866046d8df2bf1b35fad39278dbc4e7f
Mora <qiuzhongleiabc@126.com>
2017-05-11 20:20:43 +0800

    chore: 更新 ts 编译相关脚本
    在 npm scripts 中的 tsc 中指定了参数之后好像就不会使用 tsconfig 文件，
    现在改成完全使用 tsconfig 文件了，不在 tsc 命令中指定参数（除了 -w）

--- src/holiday.js
+++ src/holiday.js
@@ -1,20 +1,19 @@
 "use strict";
-exports.__esModule = true;
-var MIN_YEAR = 2010;
-var MAX_YEAR = new Date().getFullYear();
-function holiday(dateLike, lang) {
-    if (lang === void 0) { lang = 'zh_CN'; }
-    var date = typeof dateLike === 'string' ? new Date(dateLike) : dateLike;
-    var year = date.getFullYear();
-    var monthString = date.getMonth() + 1 + '';
-    var dayString = date.getDate() + '';
-    var holidayData = {};
+Object.defineProperty(exports, "__esModule", { value: true });
+const MIN_YEAR = 2010;
+const MAX_YEAR = new Date().getFullYear();
+function holiday(dateLike, lang = 'zh_CN') {
+    const date = typeof dateLike === 'string' ? new Date(dateLike) : dateLike;
+    let year = date.getFullYear();
+    let monthString = date.getMonth() + 1 + '';
+    let dayString = date.getDate() + '';
+    let holidayData = {};
     if (year >= MIN_YEAR && year <= MAX_YEAR) {
         try {
-            holidayData = require("../data/" + year + "." + lang + ".json");
+            holidayData = require(`../data/${year}.${lang}.json`);
         }
         catch (e) {
-            console.warn("\u6CA1\u6709\u5E74\u4EFD<" + year + ">\uFF0C\u8BED\u8A00<" + lang + ">\u7684\u6570\u636E\uFF0C\u8BF7\u8054\u7CFB\u7BA1\u7406\u5458\u66F4\u65B0");
+            console.warn(`没有年份<${year}>，语言<${lang}>的数据，请联系管理员更新`);
         }
     }
     if (monthString in holidayData && dayString in holidayData[monthString]) {
@@ -22,7 +21,7 @@ function holiday(dateLike, lang) {
     }
     return isWeekend(date) ? 1 : 0;
 }
-exports["default"] = holiday;
+exports.default = holiday;
 function isWeekend(date) {
     return [0, 6].includes(date.getDay());
 }
